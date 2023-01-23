a4103d109b7b3e68a3f84cd1c83f58a85f9644d5
Angie Marquina <angiemarquina65@gmail.com>
2023-01-23 14:26:47 -0500

    función + test de isMarkdown

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,4 +1,6 @@
-const { pathExists, isAbsolutePath, toAbsolute } = require('../src/api');
+const {
+  pathExists, isAbsolutePath, toAbsolute, isMarkdown,
+} = require('../src/api');
 
 describe('pathExists test', () => {
   const existPath = 'C:\\Users\\Dell\\Documents\\GitHub\\DEV001-md-links\\folder-test';
@@ -30,5 +32,17 @@ describe('toAbsolute test', () => {
 
   it('debe retornar una ruta absoluta', () => {
     expect(toAbsolute(relativePath)).toBe(absolutePath);
+  });
+});
+
+describe('isMarkdown test', () => {
+  const markdownFile = 'C:\\Users\\Dell\\Documents\\GitHub\\DEV001-md-links\\folder-test\\file-md.md';
+  const txtFile = 'C:\\Users\\Dell\\Documents\\GitHub\\DEV001-md-links\\folder-test\\file-txt.txt';
+
+  it('debe retornar true si la ruta es un archivo markdown', () => {
+    expect(isMarkdown(markdownFile)).toBe(true);
+  });
+  it('debe retornar false si la ruta no es un archivo markdown', () => {
+    expect(isMarkdown(txtFile)).toBe(false);
   });
 });
